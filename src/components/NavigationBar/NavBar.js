b2df3d6814810f390133843c65cf0387d1d74830
ezekiel444 <50213769+ezekiel444@users.noreply.github.com>
2021-01-02 23:56:37 +0100

    clean up

--- src/components/NavigationBar/NavBar.js
+++ src/components/NavigationBar/NavBar.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import * as Styles from './NavStyles'
 import Logo from "../../image/eml-logo.png";
-import { Link, NavLink } from "react-router-dom";
-import { FaBars, FaSearchengin } from 'react-icons/fa';
+import { NavLink } from "react-router-dom";
+import { FaBars } from 'react-icons/fa';
 import {AiOutlineClose} from 'react-icons/ai';
 import {IoIosHome} from 'react-icons/io';
 import { ImProfile } from "react-icons/im";
@@ -66,20 +66,3 @@ navContainerRef.current.style.height = "0px";
 
 export default NavBar;
 
-
-//  <Styles.NavHome>
-//       <NavLink to="/">
-//         <Styles.NavLogo>
-//           <img src={Logo} alt="nav logo" />
-//         </Styles.NavLogo>
-//       </NavLink>
-//       <Styles.NavSearch >
-//       <FaSearchengin className='searchIcon' color='#757575' size="1em" /> <input placeholder="search" type="text" />
-//     </Styles.NavSearch>
-
-  
-
-//  {/* <div className='mobilelayout' onClick={()=>setClick(!click)}>{click ? (<div className='navPlaceHolder'><AiOutlineClose/> <div className='formatDropDownView'>{links}</div> </div> ) : <FaBars />}</div> */}
-
-//     </Styles.NavHome>
-  
